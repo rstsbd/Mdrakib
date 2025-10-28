741035e4036ff2210e7d278664fa20ec05846bd2
rstsbd <52035574+rstsbd@users.noreply.github.com>
2025-10-29 00:17:50 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
     GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbx.../exec",
-    CONTACT_SHEETDB: "https://sheetdb.io/api/v1/YOUR_CONTACT_ID",
-    SLIDES_SHEETDB: "https://sheetdb.io/api/v1/YOUR_SLIDES_ID",
-    NEWS_SHEETDB: "https://sheetdb.io/api/v1/YOUR_NEWS_ID",
+    CONTACT_SHEETDB: "https://sheetdb.io/api/v1/CONTACT_ID",
+    SLIDES_SHEETDB: "https://sheetdb.io/api/v1/SLIDES_ID",
+    NEWS_SHEETDB: "https://sheetdb.io/api/v1/NEWS_ID",
     ADMIN: { username: "admin", password: "admin123" }
 };
